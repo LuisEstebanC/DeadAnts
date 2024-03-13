f245c90651c2c662703be60ce80c94126c22e15f
LuisEstebanC <ing2.29capellan@gmail.com>
2024-03-13 13:49:54 -0400

    Making a branch with pair programming changes to test

---
+++ DeadAntsNewApp.js
@@ -0,0 +1,25 @@
+function DeadAnts(ants){
+    if(!ants || typeof ants !=="string") return 0;
+ 
+    let countA = 0, countN = 0, countT = 0
+    const lengthAnst = ants.length
+ 
+    for (let i = 0; i < lengthAnst; i++) {
+     const currentChar = ants[i]
+     
+     if (ants[i] === "a" && ants[i+1] === "n" && ants[i+2] === "t") {
+       i += 2
+       continue
+     } // ant 
+ 
+     if(currentChar === "a") countA++
+     else if (currentChar === "n") countN++
+     else if (currentChar === "t") countT++
+ 
+   }
+ 
+    return Math.max(countA, countN, countT)
+ 
+ }
+ 
+ module.exports = DeadAnts;
--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const DeadAnts = require('./app');
+const DeadAnts = require('./DeadAntsOldApp');
 
 test('Throw an error is the input is different a string', () => {
   expect(()=>DeadAnts(4)).toThrow('You must provide a string');
